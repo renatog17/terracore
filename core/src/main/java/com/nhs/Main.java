ae313bc1ce03515e2d754f918833d6d5c1409dc1
renatog17 <renatog.oliveira17@gmail.com>
2026-04-23 02:50:07 -0300

    project set up

---
+++ core/src/main/java/com/nhs/Main.java
@@ -0,0 +1,7 @@
+package com.nhs;
+
+import com.badlogic.gdx.ApplicationAdapter;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class Main extends ApplicationAdapter {
+}
